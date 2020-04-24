3391cfcacd91a54798773efc0f1780732256e44a
Alex7498-SV <00068817@uca.edu.sv>
2020-04-24 16:37:58 -0600

    ejercicio 2 finalizado

--- tarea_4_00068817.asm
+++ tarea_4_00068817.asm
@@ -1,21 +1,26 @@
     org 100h
 
-        mov ax, 2d
-        mov bx, 2d
+        mov ax, 2d ; inicializando ax con los casos iniciales
+        mov bx, 2d ; inicializando el multiplicador
+        mov SI, 1d ; 
+        mov cx, 1d
+        mov [210h], ax ;
 
-        mov [210h], ax
-        mul bx
-        mov [211h], ax
-        mul bx
-        mov [212h], ax
-        mul bx
-        mov [213h], ax
-        mul bx
-        mov [214h], ax
-        mul bx
-        mov [215h], ax
-        mul bx
-        mov [216h], ax
-    
+prcd1:  mul bx
+        mov [SI+210h], ax ;
+        inc SI
+        inc cx
+
+        cmp  ax, 100h
+        jb prcd1
+
+prcd2:  mul bx
+        inc SI
+        mov [SI+210h], ax
+        inc cx
+        inc SI
+        
+        cmp cx, 11d
+        jb prcd2
 
     int 10h
