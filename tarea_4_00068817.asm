ce6b7b5a73d3114d7b3e0e2183b1e1d686fb8fd2
Alex7498-SV <00068817@uca.edu.sv>
2020-04-24 18:45:49 -0600

    tarea finalizada

--- tarea_4_00068817.asm
+++ tarea_4_00068817.asm
@@ -1,3 +1,65 @@
+;Ejercicio #1
+    org 100h
+        mov cx, 6d ;Se inserta el 6 en el registro cx
+        add cx, 8d ;Se le suma 8
+        add cx, 8d ;Se le suma 8
+        add cx, 1d ;Se le suma 1
+        add cx, 7d ;Se le suma 7
+        mov ax, cx ;Se inserta el contenido de cx en ax pára poder operarlo con div
+        mov cx, 5d ;Se inserta un 5 en cx
+        div cx ;Se divide el registro ax entre el registro cx
+        mov cl, "P"
+        mov [200h], cl
+        mov cl, "E"
+        mov [201h], cl
+        mov cl, "O"
+        mov [202h], cl
+        mov cl, "R"
+        mov [203h], cl
+        mov cl, " "
+        mov [204h], cl
+        mov cl, "E"
+        mov [205h], cl
+        mov cl, "S"
+        mov [206h], cl
+        mov cl, " "
+        mov [207h], cl
+        mov cl, "N"
+        mov [208h], cl
+        mov cl, "A"
+        mov [209h], cl
+        mov cl, "D"
+        mov [20Ah], cl
+        mov cl, "A"
+        mov [20Bh], cl
+
+        int 10h
+
+
+;ejercicio #2
+    org 100h
+        mov ax, 2d ; inicializando ax con los casos iniciales
+        mov bx, 2d ; inicializando el multiplicador
+        mov SI, 1d ; 
+        mov cx, 1d
+        mov [210h], ax ;
+prcd1:  mul bx
+        mov [SI+210h], ax ;
+        inc SI
+        inc cx
+        cmp  ax, 100h
+        jb prcd1
+prcd2:  mul bx
+        inc SI
+        mov [SI+210h], ax
+        inc cx
+        inc SI
+        
+        cmp cx, 11d
+        jb prcd2
+    int 10h
+
+;Ejercicio 3
     org 100h
     mov ax, 0d ; Fn-1
     mov bx, 1d ; Fn-2
